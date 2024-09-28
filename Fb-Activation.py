70a91c28ab412398d6059d6b4af85ce78d99539a
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-09-28 18:58:03 +0300

    Update Fb-Activation.py

--- Fb-Activation.py
+++ Fb-Activation.py
@@ -4,4 +4,5 @@
   "88":"Gamal",
   "11":"Ahmed",
   '100080663215433':['100080663215433','Mohamed Fathy'],
+  '223344':['100080663215433','Mohamed Fathy','https://scontent.fcai22-2.fna.fbcdn.net/v/t39.30808-1/456709006_527836049915164_5411177290995653720_n.jpg?stp=c0.122.1723.1723a_cp0_dst-jpg_s60x60&_nc_cat=105&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=c2WIpvMGboMQ7kNvgFAnlpJ&_nc_ht=scontent.fcai22-2.fna&_nc_gid=AK9SzUVxSsnQyq7UHLd-EuU&oh=00_AYC20WF819KqjyQbjidZ78xtcj9aiz2T_HSADvUbT-kr2A&oe=66FD2E32'],
 }
