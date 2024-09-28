5122417c8019fbc8726f9937fb4d6ed66a7cc6a7
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-09-28 19:16:55 +0300

    Update Fb-Activation.py

--- Fb-Activation.py
+++ Fb-Activation.py
@@ -3,4 +3,7 @@
     "77":"Mohamed",
     "88":"Gamal",
     "11":"Ahmed",
+    '100008032240221':['100008032240221','Hussein Ahmed','https://scontent.fcai22-1.fna.fbcdn.net/v/t39.30808-1/459555226_3847060428904994_4996867914164584021_n.jpg?stp=cp0_dst-jpg_s60x60&_nc_cat=102&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=UzyBRv0Q3P0Q7kNvgHJmSgt&_nc_ht=scontent.fcai22-1.fna&_nc_gid=AK9SzUVxSsnQyq7UHLd-EuU&oh=00_AYAn0ji1JpZNq61FQnTboy9YTpfw5jD8AJAAQiTEOhUnIQ&oe=66FD33FB'],
+    '100080663215433':['100080663215433','Mohamed Fathy','https://scontent.fcai22-2.fna.fbcdn.net/v/t39.30808-1/456709006_527836049915164_5411177290995653720_n.jpg?stp=c0.122.1723.1723a_cp0_dst-jpg_s60x60&_nc_cat=105&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=c2WIpvMGboMQ7kNvgFAnlpJ&_nc_ht=scontent.fcai22-2.fna&_nc_gid=AK9SzUVxSsnQyq7UHLd-EuU&oh=00_AYC20WF819KqjyQbjidZ78xtcj9aiz2T_HSADvUbT-kr2A&oe=66FD2E32'],
+    '100072354701173':['100072354701173','MuhVmed Ahmed','https://scontent.fcai22-2.fna.fbcdn.net/v/t39.30808-1/451708375_502958842125935_3709128721766674728_n.jpg?stp=cp0_dst-jpg_s60x60&_nc_cat=105&ccb=1-7&_nc_sid=50d2ac&_nc_ohc=_LRdOYRUvjcQ7kNvgHT7eqR&_nc_ht=scontent.fcai22-2.fna&_nc_gid=AK9SzUVxSsnQyq7UHLd-EuU&oh=00_AYAEe48LtEqzjmetL7ZdCjZo6cWc2ELnwPxq1h2igrP21g&oe=66FD258E'],
 }
